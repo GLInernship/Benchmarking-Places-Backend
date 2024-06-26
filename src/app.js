80573c26b0567c29a5afd2ee021ac1c8ef6235dc
NihalSaran-GL <nihal.saran@globallogic.com>
2024-06-26 21:06:19 +0530

    app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-
 // Load environment variables from .env file
 dotenv.config();
 
@@ -12,6 +11,11 @@ const app = express();
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
+
+// Respond to GET request at "/"
+app.get('/', (req, res) => {
+  res.send('WELCOME TO BENCHMARKING PLACES BACKEND');
+});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
