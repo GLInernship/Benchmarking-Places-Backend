fce6fb463256fbb1dbd74b3b4d26e21d4398014b
NihalSaran-GL <nihal.saran@globallogic.com>
2024-06-29 10:45:23 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); // Import morgan
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 
+// Middleware to log requests
+app.use(morgan('combined')); // Use morgan to log requests in 'combined' format
+
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://glinernship.github.io'], // Allow these frontend origins // Allow only your frontend origin
+  origin: ['http://localhost:3000', 'https://glinernship.github.io'], // Allow these frontend origins
   methods: ['GET', 'POST'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
 }));
@@ -34,7 +38,6 @@ const nearbyPlaceRoutes = require('./routes/nearbyPlaceRoutes');
 const gridDataRoutes = require('./routes/gridDataRoutes');
 const resultRoutes = require('./routes/resultRoutes');
 const googlePlaceRoutes = require('./routes/googlePlaceRoutes');
-
 
 // Use routes
 app.use('/api', nearbyPlaceRoutes);
